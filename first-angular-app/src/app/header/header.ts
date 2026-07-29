c89bdd102874971f5c3d00ca4ae88864e838a685
Vlado Čavrić <vladocavric@gmail.com>
2026-07-29 22:02:30 +0200

    added first component

---
+++ first-angular-app/src/app/header/header.ts
@@ -0,0 +1,9 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.html',
+  styleUrl: './header.css'
+})
+export class Header {}

609532086be06b96254d58cb5ecdb0f920ce8cd1
Vlado Cavric <vladocavric@gmail.com>
2026-08-21 15:30:01 +0200

    finished with module

--- investment-calculator-max-modules/src/app/app.component.ts
+++ investment-calculator-max-modules/src/app/app.component.ts
@@ -7,9 +7,7 @@ import { InvestmentResultsComponent } from "./investment-results/investment-resu
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
 export class AppComponent {
 

918d98b3c3a23d298e031401d76781414d8785a3
Vlado Cavric <vladocavric@gmail.com>
2026-08-21 14:58:55 +0200

    finished

--- investment-calculator-max/src/app/investment-results/investment-results.component.ts
+++ investment-calculator-max/src/app/investment-results/investment-results.component.ts
@@ -1,14 +1,7 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, inject, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { InvestmentService } from '../investment.service';
-type InvestmentData = {
-  year: number;
-  interest: number;
-  valueEndOfYear: number;
-  annualInvestment: number;
-  totalInterest: number;
-  totalAmountInvested: number;
-};
+
 @Component({
   selector: 'app-investment-results',
   standalone: true,
@@ -17,9 +10,10 @@ type InvestmentData = {
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-  investmentService = inject(InvestmentService);
+  private investmentService = inject(InvestmentService);
 
-  get results() {
-    return this.investmentService.resultsData();
-  }
+  results = this.investmentService.resultsData.asReadonly()
+  // results = computed(() => this.investmentService.resultsData())
 }
+
+

b4f47b6723159989cb94d83588e8bd9266876325
Vlado Cavric <vladocavric@gmail.com>
2026-08-30 22:25:53 +0200

    directives 1st commit

--- custom-2w-binding-starting-project/src/app/rect/rect.component.ts
+++ custom-2w-binding-starting-project/src/app/rect/rect.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, model, Output } from '@angular/core';
 
 @Component({
   selector: 'app-rect',
@@ -9,8 +9,10 @@ import { Component } from '@angular/core';
 })
 export class RectComponent {
   // Todo: Implement custom two-way binding
-
+  // @Input({ required: true }) size!: { width: string; height: string }
+  // @Output() sizeChange = new EventEmitter<{ width: string; height: string }>();
+  size = model.required<{ width: string; height: string }>()
   onReset() {
-    // ...
+    this.size.set({ width: '100', height: '100' })
   }
 }

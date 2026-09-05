1b6811d766f5adf3da34fb0615ca085c12e8dbfb
Vlado Cavric <vladocavric@gmail.com>
2026-09-05 21:51:04 +0200

    forms first commit

--- http/src/app/places/available-places/available-places.component.ts
+++ http/src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { HttpClient } from '@angular/common/http';
+import { catchError, map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +13,55 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isLoading = signal<boolean>(false);
+  error = signal<string | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit() {
+    this.isLoading.set(true);
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places').pipe(
+        map((resData) => resData.places),
+        catchError((err) => {
+          console.error(err);
+          throw Error('Failed to fetch places. Please try again later.');
+
+        }))
+      .subscribe({
+        next: (resData) => {
+          this.places.set(resData);
+          console.log(resData);
+        },
+        complete: () => {
+          this.isLoading.set(false);
+        },
+        error: (err) => {
+          this.error.set(err.message || 'An error occurred while fetching places.');
+          this.isLoading.set(false);
+        },
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
+
+  onSelectPlace(place: Place) {
+    console.log('Selected place:', place);
+    this.httpClient.put(`http://localhost:3000/user-places`, { placeId: place.id }).subscribe({
+      next: (resData) => {
+        console.log('Place updated successfully:', resData);
+      },
+      error: (err) => {
+        console.error('Error updating place:', err);
+      },
+      complete: () => {
+        console.log('Update request completed.');
+      }
+    });
+  }
 }
--- http/src/app/places/user-places/user-places.component.ts
+++ http/src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-places',
@@ -11,4 +13,32 @@ import { PlacesComponent } from '../places.component';
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent {
+  places = signal<Place[]>([]);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+  ngOnInit() {
+    const subscription = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/user-places').subscribe({
+        next: (resData) => {
+          console.log('User places fetched successfully:', resData.places);
+          this.places.set(resData.places);
+        }
+      });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
+
+  onDeletePlace(place: Place) {
+    this.httpClient.delete(`http://localhost:3000/user-places/${place.id}`).subscribe({
+      next: (resData) => {
+        console.log('Place deleted successfully:', resData);
+        this.places.update((currentPlaces) => currentPlaces.filter((p) => p.id !== place.id));
+      },
+      error: (err) => {
+        console.error('Error deleting place:', err);
+      },
+    });
+  }
 }
